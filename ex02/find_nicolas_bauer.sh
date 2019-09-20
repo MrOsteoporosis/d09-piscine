f3497144d4aa31fb2d0a5465a73c55138d40ba2f
Sander Verheijen <sanderverheijen@outlook.com>
2019-09-20 11:19:59 +0200

    ex02 only nicolasbauer -i)

--- ex02/find_nicolas_bauer.sh
+++ ex02/find_nicolas_bauer.sh
@@ -1,3 +1,3 @@
-cat $1 | grep -i "nicolas" | grep -i "bauer" | grep -o "([0-9]\{3\})[0-9]\{3\}[0-9]\{4\}\s"
-cat $1 | grep -i "bauer" | grep -o "[0-9]\{3\}-[0-9]\{3\}-[0-9]\{2\}\s"
-cat $1 | grep -i "nicolas" | grep -i "bauer" | grep -o "[0-9]\{1\}-[0-9]\{3\}-[0-9]\{3\}-[0-9]\{4\}\s"
+cat $1 | grep -i "nicolas\sbauer" | grep -o '([0-9]\{3\})[0-9]\{3\}\-[0-9]\{4\}'
+cat $1 | grep -i "nicolas\sbauer" | grep -o '[0-9]\{3\}\-[0-9]\{3\}\-[0-9]\{2\}'
+
